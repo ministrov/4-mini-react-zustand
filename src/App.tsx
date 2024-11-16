d3409d6fd7a19e1e2cea79d07c172a50d3738051
ministrov <61511030+ministrov@users.noreply.github.com>
2024-11-16 16:38:28 +0300

    feat: add todo to array of todos, without store

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,47 @@
-import { getTen } from './helpers/addTen';
-import { useCounterStore } from './model/counterStore'
+import { useState } from 'react';
+import { TodoState } from './model/todoStore'
+import { Card, Checkbox, Input, Button, Tooltip } from 'antd';
+import { SearchOutlined } from '@ant-design/icons';
+// import { getTen } from './helpers/addTen';
+// import { useCounterStore } from './model/counterStore'
 import './App.css'
 
 function App() {
-  const { counter, increment, decrement } = useCounterStore();
+  const [todos, setTodos] = useState<TodoState[] | []>([]);
+  const [value, setValue] = useState<string>('');
+  // const todos: TodoState[] = [];
+  // const { counter, increment, decrement } = useCounterStore();
+
+  const addTodo = () => {
+    if (todos.length === 0) {
+      setTodos((prevState) => [...prevState, { id: Math.trunc((Math.random() * 10) + 1), title: value, isCompleted: true }]);
+    } else {
+      // setTodos([item]);
+    }
+  }
 
   return (
     <div className='wrapper'>
-      <button onClick={increment}>+</button>
+      <div className='wrapper__header'>
+        <Input style={{ width: '350px' }} onChange={(e) => setValue(e.target.value)} placeholder='Введите значение' value={value} required />
+        <Tooltip title="search">
+          <Button shape="circle" icon={<SearchOutlined />} onClick={addTodo}/>
+        </Tooltip>
+      </div>
+      {/* <button onClick={increment}>+</button>
       <span>{counter}</span>
       <button onClick={decrement}>-</button>
-      <button onClick={getTen}>add 10</button>
+      <button onClick={getTen}>add 10</button> */}
+      <ul className='wrapper__list'>
+        {todos.length > 0 && todos.map((todo: TodoState) => (
+          <Card key={todo.id} className='card'>
+            <Checkbox checked={todo.isCompleted} />
+            <span>{todo.title}</span>
+          </Card>
+        ))}
+
+        {todos.length === 0 && <p>Нужно ввести название таски</p>}
+      </ul>
     </div>
   )
 }
---
+++ src/model/todoStore.ts
@@ -0,0 +1,6 @@
+export type TodoState = {
+  id: number;
+  title: string;
+  isCompleted: boolean;
+}
+
