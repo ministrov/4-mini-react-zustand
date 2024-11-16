692f0314e11440841b002bc8923fafd5f62374b0
ministrov <61511030+ministrov@users.noreply.github.com>
2024-11-16 13:57:12 +0300

    feat: initialize store and create counterSlice to increment or decrement counter state

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,15 @@
 import './App.css'
+import { useCounterStore } from './model/counterStore'
 
 function App() {
+  const { counter, increment, decrement } = useCounterStore();
 
   return (
-    <div className='wrapper'></div>
+    <div className='wrapper'>
+      <button onClick={increment}>+</button>
+      <span>{counter}</span>
+      <button onClick={decrement}>-</button>
+    </div>
   )
 }
 
